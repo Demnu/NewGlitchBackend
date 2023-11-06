ad633e47165c8d4dfe5b662c98f4233624639517
Demnu <harrison.joe.collins@gmail.com>
2023-11-06 23:56:13 +1100

    autogen swagger update

--- swagger.tsx
+++ swagger.tsx
@@ -32,13 +32,9 @@ const doc = {
 };
 
 const developmentLocation = './src/swagger_output.json';
-
-const productionLocation = './dist/src/swagger_output.json';
 const routes = ['./src/app.tsx'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
 swaggerAutogen(developmentLocation, routes, doc);
-
-swaggerAutogen(productionLocation, routes, doc);

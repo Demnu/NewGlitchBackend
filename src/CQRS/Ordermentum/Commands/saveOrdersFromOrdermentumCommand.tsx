f81fd05a49d2a1421ba9f81e8f05c31a15c65faa
Demnu <harrison.joe.collins@gmail.com>
2023-11-07 10:49:27 +1100

    lil edit

--- src/CQRS/Ordermentum/Commands/saveOrdersFromOrdermentumCommand.tsx
+++ src/CQRS/Ordermentum/Commands/saveOrdersFromOrdermentumCommand.tsx
@@ -136,7 +136,7 @@ const formatOrdersFromOrdermentum = (
 const downloadOrdersFromOrdermentum = async () => {
   // Set your custom pagination settings
   const customPagination = {
-    pageSize: 40,
+    pageSize: 30,
     pageNo: 1,
     filter: 'b0d7ba3e-4ab1-4c1b-85ec-1275cd6687b9'
   };

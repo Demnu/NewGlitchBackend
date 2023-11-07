65c2a9cec9b094f8c7831a36548133b2d0676518
Demnu <harrison.joe.collins@gmail.com>
2023-11-08 02:38:05 +1100

    update create recipe schema

--- src/CQRS/Recipes/Commands/createRecipeRequestDto.tsx
+++ src/CQRS/Recipes/Commands/createRecipeRequestDto.tsx
@@ -21,8 +21,8 @@ type CreateRecipeRequestDto = z.infer<typeof CreateRecipeRequestDtoSchema>;
 const CreateRecipeRequestDtoJsonSchema = {
   $productId: '270551eb-4b20-43c1-9258-f474bb9745bc',
   $recipeName: 'Yummy recipe',
-  $existingBeans: [{ beanId: 3, $beanAmount: 200 }],
-  $newBeans: [{ beanName: 'New Bean', beanAmount: 400 }]
+  $existingBeans: [{ $beanId: 3, $beanAmount: 200 }],
+  $newBeans: [{ $beanName: 'New Bean', $beanAmount: 400 }]
 };
 export {
   CreateRecipeRequestDto,
